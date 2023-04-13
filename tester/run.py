f4d3d26bc4059db87e992496dd88a6145169c138
Jasper van Merle <jaspervmerle@gmail.com>
2023-04-14 01:44:17 +0200

    Use HCR in runner

--- tester/run.py
+++ tester/run.py
@@ -1,88 +1,44 @@
-import argparse
-import multiprocessing
-import json
+import hcr
 import re
-import subprocess
 from pathlib import Path
-from typing import List
 
-def update_overview() -> None:
-    scores_by_solver = {}
-    results_root = Path(__file__).parent / "results"
+PROJECT_ROOT = Path(__file__).parent.parent
 
-    for directory in results_root.iterdir():
-        scores_by_seed = {}
+solvers = sorted([file.stem for file in (PROJECT_ROOT / "build" / "libs").iterdir()])
+default_seeds = list(range(1, 101))
+is_maximizing = True
+results_directory = PROJECT_ROOT / "tester" / "results"
 
-        for file in directory.iterdir():
-            if file.name.endswith(".txt"):
-                scores_by_seed[file.stem] = int(file.read_text(encoding="utf-8").strip())
+def run_solvers(solver: str, inputs: list[int]) -> list[hcr.Output]:
+    solver_jar = PROJECT_ROOT / "build" / "libs" / f"{solver}.jar"
 
-        scores_by_solver[directory.name] = scores_by_seed
+    with hcr.temporary_directory() as output_directory:
+        process = hcr.run_process([
+            "java",
+            "-jar", PROJECT_ROOT / "tester" / "tester.jar",
+            "-exec", f"/home/jasper/.sdkman/candidates/java/8.0.345-tem/bin/java -jar {solver_jar}",
+            "-seed", "{" + ",".join(map(str, inputs)) + "}",
+            "-threads", hcr.thread_count(len(inputs)),
+            "-novis",
+            "-saveSolOutput",
+            "-saveSolError",
+        ], cwd=output_directory)
 
-    overview_template_file = Path(__file__).parent / "overview.tmpl.html"
-    overview_file = Path(__file__).parent / "overview.html"
+        outputs = []
+        for input in inputs:
+            stdout = (output_directory / f"{input}.out").read_text(encoding="utf-8")
+            stderr = (output_directory / f"{input}.err").read_text(encoding="utf-8")
 
-    overview_template = overview_template_file.read_text(encoding="utf-8")
-    overview = overview_template.replace("/* scores_by_solver*/{}", json.dumps(scores_by_solver))
+            pattern_prefix = f"Seed = {input}, " if len(inputs) > 1 else ""
+            score = re.search(pattern_prefix + r"Score = (-?\d+)\.0", process.stdout).group(1)
+            stderr += f"Score = {score}\n"
 
-    with overview_file.open("w+", encoding="utf-8") as file:
-        file.write(overview)
+            outputs.append(hcr.Output(stdout, stderr))
 
-    print(f"Overview: file://{overview_file}")
+        return outputs
 
-def run(solver: Path, seeds: List[int], results_directory: Path) -> None:
-    args = [
-        "java",
-        "-jar", str(Path(__file__).parent / "tester.jar"),
-        "-exec", f"/home/jasper/.sdkman/candidates/java/8.0.345-tem/bin/java -jar {str(solver)}",
-        "-seed", "{" + ",".join(map(str, seeds)) + "}",
-        "-threads", str(min(len(seeds), multiprocessing.cpu_count() - 2)),
-        "-novis",
-        "-saveSolInput",
-        "-saveSolOutput",
-        "-saveSolError"
-    ]
-
-    if not results_directory.is_dir():
-        results_directory.mkdir(parents=True)
-
-    process = subprocess.run(args, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, cwd=results_directory)
-
-    if process.returncode != 0:
-        print(process.stdout)
-        raise RuntimeError(f"Tester exited with status code {process.returncode}")
-
-    output = process.stdout.decode("utf-8")
-
-    for seed in seeds:
-        pattern_prefix = f"Seed = {seed}, " if len(seeds) > 1 else ""
-        score = re.search(pattern_prefix + r"Score = (-?\d+)\.0", output).group(1)
-
-        print(f"{seed}: {score}")
-
-        score_file = results_directory / f"{seed}.txt"
-        with score_file.open("w+", encoding="utf-8") as file:
-            file.write(score)
-
-def main() -> None:
-    parser = argparse.ArgumentParser(description="Run a solver.")
-    parser.add_argument("solver", type=str, help="the solver to run")
-    parser.add_argument("--seed", type=int, help="the seed to run (defaults to 1-100)")
-
-    args = parser.parse_args()
-
-    solver = Path(__file__).parent.parent / "build" / "libs" / f"{args.solver}.jar"
-    if not solver.is_file():
-        raise RuntimeError(f"Solver not found, {solver} is not a file")
-
-    results_directory = Path(__file__).parent / "results" / args.solver
-
-    if args.seed is None:
-        run(solver, list(range(1, 101)), results_directory)
-    else:
-        run(solver, [args.seed], results_directory)
-
-    update_overview()
+def get_score(output: hcr.Output) -> float:
+    return float(output.stderr.splitlines()[-1].split(" = ")[1])
 
 if __name__ == "__main__":
-    main()
+    hcr.cli(solvers, default_seeds, is_maximizing, results_directory, get_score, run_solvers=run_solvers)
